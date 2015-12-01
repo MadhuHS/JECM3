5141423ad8e154186122225ea7dd6af90c2ddd89
MadhuHS <madhuhs943@gmail.com>
2015-12-01 10:51:10 +0530

    Multiple Window Fragments
    01/12/2015

--- Dynamic_Fragments/src/com/jsp/dynamic_fragments/MainActivity.java
+++ Dynamic_Fragments/src/com/jsp/dynamic_fragments/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
 		
 		startbutton = (Button) findViewById(R.id.buttonaddfragment);
