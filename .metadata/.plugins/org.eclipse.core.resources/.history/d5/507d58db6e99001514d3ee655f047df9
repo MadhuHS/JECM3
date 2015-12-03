da13a02ea5552fd79374f9c00eaaf0ce4cd1aa4f
MadhuHS <madhuhs943@gmail.com>
2015-12-03 10:18:01 +0530

    Codes till Notifications
    03/12/2015

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d5/507d58db6e99001514d3ee655f047df9
@@ -0,0 +1,15 @@
+package com.example.broadcastrecvr_demo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyReciever extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) 
+	{
+		
+	}
+
+}
--- BroadcastRecvr_Demo/src/com/example/broadcastrecvr_demo/MyReciever.java
+++ BroadcastRecvr_Demo/src/com/example/broadcastrecvr_demo/MyReciever.java
@@ -3,13 +3,14 @@ package com.example.broadcastrecvr_demo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MyReciever extends BroadcastReceiver {
 
 	@Override
-	public void onReceive(Context arg0, Intent arg1) 
+	public void onReceive(Context context, Intent arg1) 
 	{
-		
+		Toast.makeText(context, "Phone State is changed",Toast.LENGTH_SHORT).show();
 	}
 
 }
