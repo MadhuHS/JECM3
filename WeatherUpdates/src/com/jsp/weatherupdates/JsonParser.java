98f509b894a48c07ffe3114731e9f86e7e82ecb1
MadhuHS <madhuhs943@gmail.com>
2015-12-17 00:06:11 +0530

    codes till json parsing
    17/12/2015

---
+++ WeatherUpdates/src/com/jsp/weatherupdates/JsonParser.java
@@ -0,0 +1,39 @@
+package com.jsp.weatherupdates;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class JsonParser 
+{
+	static Bundle b1;
+
+    public static Bundle processjson(String jsondata)
+    {
+    	
+    	try 
+    	{
+			JSONObject rootobject = new JSONObject(jsondata);
+			
+			Log.d("[DEBUG]",rootobject.getString("name"));
+			
+			
+			JSONObject mainobject = rootobject.getJSONObject("main");
+			Log.d("[DEBUG]",""+mainobject.getDouble("temp"));
+			
+			b1 = new Bundle();
+			b1.putDouble("temp", mainobject.getDouble("temp"));
+	
+		} 
+    	
+    	catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	return b1;
+    }
+
+}
